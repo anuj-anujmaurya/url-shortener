966da68af4b904db392b78247dbd81d5b3f277fe
Anuj Maurya <anuj.anujmaurya@gmail.com>
2024-03-29 21:20:21 +0530

    added sql connection, and models for urls

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // application struct to hold the app's dependencies for web app.
@@ -29,6 +33,24 @@ func main() {
 	// Create loggers
 	infoLog := log.New(infoLogFile, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(errorLogFile, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	username := "user"
+	password := "******"
+	dbName := "url_map"
+	dbHost := "localhost"
+	dbPort := "3306"
+
+	// Create a DSN (Data Source Name) string
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, dbHost, dbPort, dbName)
+
+	// Open a connection to the database
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		errorLog.Println("Error opening database connection:", err)
+		return
+	}
+
+	defer db.Close()
 
 	// initialize new instance of app struct (containing the dependencies)
 	app := &application{
---
+++ internal/models/urls.go
@@ -0,0 +1,69 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+// define url type to hold the individual row data
+type Url struct {
+	ID       int
+	LongURL  string
+	ShortURL string
+	Created  time.Time
+}
+
+// wrap sql.DB connection pool
+type URLModel struct {
+	DB *sql.DB
+}
+
+// check if long url exist in db, return short
+func (m *URLModel) CheckIfExists(longURL string) (shortURL string, err error) {
+	stmt := `SELECT short_url FROM url_map WHERE long_url = ?`
+	row := m.DB.QueryRow(stmt, longURL)
+
+	var short_url string
+
+	err = row.Scan(&short_url)
+	if err != nil {
+		// no record found
+		if err == sql.ErrNoRows {
+			return "", nil
+		}
+		return "", err
+
+	}
+
+	return short_url, nil
+}
+
+// insert the long url, return id
+func (m *URLModel) Insert(longURL string) (insertID int64, err error) {
+	stmt := `INSERT INTO url_map (longURL, created) VALUES (?, UTC_TIMESTAMP())`
+	result, err := m.DB.Exec(stmt, longURL)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+	return int64(id), nil
+
+}
+
+// update with id,
+func (m *URLModel) UpdateShortURL(insertID int64, shortURL string) error {
+	stmt := `UPDATE url_map SET short_url = ? WHERE id = ?`
+	_, err := m.DB.Exec(stmt, shortURL, insertID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
