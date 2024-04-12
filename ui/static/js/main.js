c97ba6066fd2730777de6436db98ddf918124d07
Anuj Maurya <anuj.anujmaurya@gmail.com>
2024-04-12 19:11:29 +0530

    added flash msg after copying url

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -38,3 +38,29 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	}
 });
+
+// copy the short url to clipboard
+// navigator.clipboard.writeText
+let copyIcon = document.getElementById('copyIcon');
+if (copyIcon) {
+	copyIcon.addEventListener('click', function () {
+		let copyItem = document.getElementById('short_url').value;
+		// copy only when the url has been shorted
+		if (copyItem.length) {
+			navigator.clipboard.writeText(copyItem);
+			// now also show a msg that value has been copied to clipboard
+			var flashMsg = document.createElement('div');
+			flashMsg.classList.add('alert', 'alert-success');
+			flashMsg.textContent = 'The short URL has been copied to your clipboard.';
+
+			var form = document.getElementById('url_form');
+			form.parentNode.insertBefore(flashMsg, form);
+
+			// remove it after 3 seconds
+			setTimeout(function () {
+				flashMsg.parentNode.removeChild(flashMsg);
+			}, 3000);
+		}
+	});
+}
+
